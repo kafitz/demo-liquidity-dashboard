997ef449282af7b6ad08d39350c8c4c83eda9a60
K Fitzsimmons <kafitz22@gmail.com>
2021-11-01 23:02:22 -0400

    Comments and attribution

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,7 @@
+/* ./lib/utils.ts */
+
+// Format money into more human-readable groupings
+// https://stackoverflow.com/a/55987576
 export const formatCash = (n: number) => {
     if (n < 1e3) return n;
     if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'K';
